2f8c113657114acf8ed3e9ff6741d38100755a18
samhamm <samhamm@gmail.com>
2015-02-03 12:07:59 -0800

    now receives multiple names from the command line

--- test/greet-test.js
+++ test/greet-test.js
@@ -5,13 +5,15 @@
 
 var expect = require('chai').expect;
 var greet = require('../lib/greet');
+var people;
 
 describe('greet', function(name) {
   it('should greet people by name', function() {
     expect(greet.to(name)).to.eql('hello ' + name);
   });
 
-  it('should take a name to greet from cmd line', function() {
-    expect(greet.to(process.argv.slice(2))).to.eql('hello ' + process.argv.slice(2));
+  it('should take names to greet from cmd line', function() {
+    expect(greet.toAll(people)).to.eql('hello ' + people);
   });
 });
+
