9f85171bd6a7cdcf4a14c60c1a257a8ad491f14a
samhamm <samhamm@gmail.com>
2015-02-03 11:11:10 -0800

    refactored to utilize 'exports = module.exports'

--- test/greet-test.js
+++ test/greet-test.js
@@ -5,14 +5,13 @@
 
 var expect = require('chai').expect;
 var greet = require('../lib/greet');
-var name;
 
-describe('greet', function() {
+describe('greet', function(name) {
   it('should greet people by name', function() {
-    expect(greet(name)).to.eql('hello ' + name);
+    expect(greet.to(name)).to.eql('hello ' + name);
   });
 
   it('should take a name to greet from cmd line', function() {
-    expect(greet(process.argv.slice(2))).to.eql('hello ' + process.argv.slice(2));
+    expect(greet.to(process.argv.slice(2))).to.eql('hello ' + process.argv.slice(2));
   });
 });
